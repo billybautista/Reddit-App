f683d72abee41228afb3c7cc5bea3f0c3530703f
billybautista <billy.bautista.cornelio@gmail.com>
2021-08-23 16:09:39 -0300

    First Commit

---
+++ App/src/Components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function PostCard() {
+  return (
+    <View>
+      <Text>Post Card</Text>
+    </View>
+  );
+}
+
+export default PostCard;
